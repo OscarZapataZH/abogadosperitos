4c6d3ee41766705fda91ca35e9d93e0f1b32d40b
Oscar <zapatahdez98@hotmail.com>
2019-10-22 13:58:02 -0500

    php

--- index.php
+++ index.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
     <center>
     <img src="construct.jpg" alt="80%" width="80%">
-
+    <br>
+    <button type="button" class="btn btn-success btn-lg">Prospectos</button>
 </center>
 </body>
 </html>
